b3f1bb099dc34166b6398711907657e07fe84a8d
Kyryl <91323838+kirizdev@users.noreply.github.com>
2023-01-10 18:34:34 +0000

    Update init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,5 +1,5 @@
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.number = true
 vim.opt.termguicolors = true
@@ -11,29 +11,29 @@ local map = vim.api.nvim_set_keymap
 map("n", "<leader>o", "<Cmd>NvimTreeOpen<CR>", {noremap = true})
 
 require("packer").startup(function (use)
-	use "wbthomason/packer.nvim"
+  use "wbthomason/packer.nvim"
 
-	use {
-		"RRethy/nvim-base16",
-		config = function()
-			require("base16-colorscheme").setup("theme")
-		end
-	}
+  use {
+    "RRethy/nvim-base16",
+    config = function()
+      require("base16-colorscheme").setup("theme")
+    end
+  }
 
-	use {
-		"nvim-tree/nvim-tree.lua",
-		requires = {
-			"nvim-tree/nvim-web-devicons",
-		},
-		config = function()
-			require("nvim-tree").setup()	
-		end
-	}
-	
-	use {
-		"nvim-treesitter/nvim-treesitter",
-		run = function()
-			require("nvim-treesitter").setup()
-		end
-	}
+  use {
+    "nvim-tree/nvim-tree.lua",
+    requires = {
+      "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+      require("nvim-tree").setup()  
+    end
+  }
+  
+  use {
+    "nvim-treesitter/nvim-treesitter",
+    run = function()
+      require("nvim-treesitter").setup()
+    end
+  }
 end)
